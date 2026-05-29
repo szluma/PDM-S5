731085ad4c8c7fcea0c8c8c825c0e9193e837c78
luma <lumadesouzaferreira1@gmail.com>
2026-05-29 11:36:21 -0300

    Add files via upload

--- Banco de Dados/src/app/index.tsx
+++ Banco de Dados/src/app/index.tsx
@@ -1,7 +1,27 @@
-import {View} from 'react-native';
+import { View, ActivityIndicator, Text } from 'react-native';
 import CadastroAluno from '../modules/alunos/cadastroAluno';
+import { initializeDatabase } from '@/database/database';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+    const [dbinicialized, setDBinicialized] = useState(false);
+    useEffect(() => {
+        try{
+            // initializeDatabase();
+            setDBinicialized(true);
+        }catch(error){
+            console.error('Erro ao inicializar o banco de dados: ', error)
+        }
+    }, []);
+
+    if(!dbinicialized){
+        return (
+        <View>
+            <ActivityIndicator size="large" color="#0f659e"/>
+            <Text>Inicializando bando de dados...</Text>
+        </View>
+        );
+    }
     return(
         <View>
             <CadastroAluno/>
--- Banco de Dados/src/modules/alunos/cadastroAluno.tsx
+++ Banco de Dados/src/modules/alunos/cadastroAluno.tsx
@@ -3,16 +3,42 @@ import {Alert,
         TextInput,
         TouchableOpacity,
         View} from "react-native";
-        import {Styles} from ../style
 import { styles } from "./style";
+import { AlunoService } from "./alunoService";
+import { useState } from "react";
 
 export default function CadastroAluno(){
+    const [nome, setNome] = useState('');
+     const [cgu, setcgu] = useState('');
+
+    const handleSalvar = () => {
+        // Lógica para salva o aluno usando AlunoService.inserir(nome)
+
+        try{
+            AlunoService.inserir(nome,cgu);
+            setNome('');
+            setcgu('');
+        }catch(error){
+            console.error('Erro ao salvar o aluno:', error);
+        }
+    };
+
+
     return(
         <View style={styles.container}>
             <Text style ={styles.title}>Cadastro de Aluno</Text>
 
-            <TextInput style={styles.input}placeholder="Nome do Aluno"/>
-            <TextInput style={styles.input}placeholder="CGU do Aluno"/>
+            <TextInput 
+            style={styles.input} 
+            value={nome} 
+            placeholder="Nome do Aluno" 
+            onChangeText={setNome}/>
+
+            <TextInput 
+            style={styles.input} 
+            value={cgu}
+            placeholder="CGU do Aluno"
+            onChangeText={setcgu}/>
 
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonTetx}>Salvar</Text>
@@ -20,4 +46,3 @@ export default function CadastroAluno(){
         </View>
     );
 }
-
